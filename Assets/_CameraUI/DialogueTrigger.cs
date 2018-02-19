85649afe35231c6c4c5128bd37df9c4207714e02
b <biro123@ntlworld.com>
2018-02-19 00:35:57 +0000

    Added Dialogue Triggers for comments/quests

---
+++ Assets/_CameraUI/DialogueTrigger.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using RPG.Characters;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] Text textBox;
+    [SerializeField] string[] textLinesToDisplay;
+    [SerializeField] float displayTime = 3f;
+    [SerializeField] float triggerRadius = 5f;
+    [SerializeField] bool isOneTimeOnly = true;
+
+    bool hasPlayed = false;
+    bool isPlaying = false;
+    GameObject player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControl>().gameObject;
+    }
+
+    private void Update()
+    {
+        var distanceToPlayer = Vector3.Magnitude(transform.position - player.transform.position);
+        if(distanceToPlayer <= triggerRadius)
+        {
+            RequestDisplayText();
+        }
+    }
+
+    void RequestDisplayText()
+    {
+        if (isOneTimeOnly && hasPlayed)
+        {
+            return;
+        }
+        else if (!isPlaying)   
+        {
+            StartCoroutine(DisplayTextOverTime());
+            hasPlayed = true;
+        }
+    }
+
+    private IEnumerator DisplayTextOverTime()
+    {
+        isPlaying = true;
+
+        foreach (string textLineToDisplay in textLinesToDisplay)
+        {
+            textBox.text = textLineToDisplay;
+            yield return new WaitForSeconds(displayTime);
+            textBox.text = null;
+        }
+        isPlaying = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0f, 255f, .5f);
+        Gizmos.DrawWireSphere(transform.position, triggerRadius);
+    }
+}
